36447ce203be17b6b2829203872b88cf3bd40acb
Oli Thompson <olithompson@rocketmail.com>
2019-11-29 18:44:29 +0000

    Add Checker message

--- Emailer.py
+++ Emailer.py
@@ -12,14 +12,21 @@ server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 smtp_server = "smtp.gmail.com"
 sender_email = "isstracker2019@gmail.com"  # Enter your address
-receiver_email = "olithompson@rocketmail.com"  # Enter receiver address
+receiver_email = "olithompson1919@gmail.com"  # Enter receiver address
 password = 'jsRfpcy9'
 server.login(sender_email, password)
-server.sendmail(sender_email, receiver_email, "Tracker turned on")
+message = """\
+Subject: ISS Tracker
+
+Tracker turned on"""
+server.sendmail(sender_email, receiver_email, message)
 
 while True:
     data = findISS()
     if (Latmin < float(data['iss_position']['latitude']) < Latmax) and (Longmin < float(data['iss_position']['latitude']) < Longmax):
-        message = "ISS directly overhead" 
+        mmessage = """\
+        Subject: ISS Tracker
+
+        ISS directly overhead"""
         server.sendmail(sender_email, receiver_email, message)
 
